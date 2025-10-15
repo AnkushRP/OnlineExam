4b472003f7cbcef5901acda8016dd3fe442d5116
AnkushRP <147685188+AnkushRP@users.noreply.github.com>
2025-10-15 18:55:27 +0530

    Update sql.php

--- sql.php
+++ sql.php
@@ -1,7 +1,8 @@
 <?php 
 $host="localhost";//default for xampp
 $user="root";//default for xampp
-$project="projet";//db ucreate in phpmyadmin
+$project="projet";//db you  create in phpmyadmin
 $ps="";//default for xampp
 ?>
+
 
